0ac712de0ae3043715bda0d6bdc3ce04f38eca53
Grini Youssef <grini.youssef@gmail.com>
2020-12-23 14:22:31 +0100

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'E-Investissement';
+
+  ngOnInit(): void {
+    var items = document.querySelectorAll('.nav-item');
+    items.forEach(function (item, index) {
+      item.addEventListener('click', function (e) {
+        console.log("grini test");
+        var indicator = <HTMLElement> document.querySelector('.is-active');
+        indicator.classList.remove('is-active');
+        item.classList.add('is-active');
+      });
+    });
+  }
+
 }
