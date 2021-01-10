1995e5a8032c58ff31e03a10ade2364a2b40377e
Grini Youssef <grini.youssef@gmail.com>
2021-01-10 16:27:16 +0100

    add baseUrl for deployement

--- src/app/comment/comment.service.ts
+++ src/app/comment/comment.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { CommentPayload } from './comment.payload';
 
 @Injectable({
@@ -8,18 +9,20 @@ import { CommentPayload } from './comment.payload';
 })
 export class CommentService {
 
+  baseUrl = environment.baseUrl; 
+
   constructor(private httpClient: HttpClient) { }
 
   getAllCommentsForPost(postId: number): Observable<CommentPayload[]> {
-    return this.httpClient.get<CommentPayload[]>('http://localhost:8080/api/comments/byIdee/' + postId);
+    return this.httpClient.get<CommentPayload[]>( this.baseUrl + 'api/comments/byIdee/' + postId);
   }
 
   postComment(commentPayload: CommentPayload): Observable<any> {
-    return this.httpClient.post<any>('http://localhost:8080/api/comments/', commentPayload);
+    return this.httpClient.post<any>( this.baseUrl + 'api/comments/', commentPayload);
   }
 
   getAllCommentsByUser(name: string) {
-    return this.httpClient.get<CommentPayload[]>('http://localhost:8080/api/comments/byUser/' + name);
+    return this.httpClient.get<CommentPayload[]>( this.baseUrl + 'api/comments/byUser/' + name);
   }
 
 }
--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { CreatePostPayload } from '../post/create-post/create-post.payload';
 import { IdeeModel } from './idee-model';
 
@@ -9,22 +10,24 @@ import { IdeeModel } from './idee-model';
 })
 export class PostService {
 
+  baseUrl = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getAllPosts(): Observable<Array<IdeeModel>> {
-    return this.http.get<Array<IdeeModel>>('http://localhost:8080/api/idees');
+    return this.http.get<Array<IdeeModel>>( this.baseUrl + 'api/idees');
   }
 
   createPost(postPayload: CreatePostPayload): Observable<any> {
-    return this.http.post('http://localhost:8080/api/idees', postPayload);
+    return this.http.post( this.baseUrl + 'api/idees', postPayload);
   }
 
   getIdee(id: number): Observable<IdeeModel> {
-    return this.http.get<IdeeModel>('http://localhost:8080/api/idees/' + id);
+    return this.http.get<IdeeModel>( this.baseUrl + 'api/idees/' + id);
   }
 
   getAllPostsByUser(name: string): Observable<IdeeModel[]> {
-    return this.http.get<IdeeModel[]>('http://localhost:8080/api/idees/by-user/' + name);
+    return this.http.get<IdeeModel[]>( this.baseUrl + 'api/idees/by-user/' + name);
   }
 
 }
